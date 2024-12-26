6af1d9c6548d3b116d940466a261227d20f18816
Jeremy Chauvet <jer.chauvet@gmail.com>
2024-12-26 14:54:24 +0100

    feat: add support of custom domain names

--- vault_pki_secret_backend_cert.tf
+++ vault_pki_secret_backend_cert.tf
@@ -3,7 +3,7 @@ resource "vault_pki_secret_backend_cert" "certificate" {
   issuer_ref            = vault_pki_secret_backend_issuer.intermediate.issuer_id
   backend               = vault_pki_secret_backend_role.intermediate_ca.backend
   name                  = vault_pki_secret_backend_role.intermediate_ca.name
-  common_name           = each.value.do_not_append_root_domain_name == null || true ? each.value.name : "${each.value.name}.${var.root_domain_name}"
+  common_name           = each.value.do_not_append_root_domain_name == true ? each.value.name : "${each.value.name}.${var.root_domain_name}"
   ttl                   = each.value.ttl_in_seconds
   revoke                = each.value.revoke_on_destroy
 }
